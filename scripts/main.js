717c492aab0297eaf988d67d300d485744749297
amandashmore <133187866+amandashmore@users.noreply.github.com>
2024-01-26 17:34:58 -0500

    added stuff to js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
+//myImage.onclick = () => {
+ // const mySrc = myImage.getAttribute("src");
+//  if (mySrc === "images/firefox-icon.png") {
+  //  myImage.setAttribute("src", "images/yellowlab.jpeg");
+ // } else {
+ //   myImage.setAttribute("src", "images/blacklab.jpeg");
+ // }
+//};

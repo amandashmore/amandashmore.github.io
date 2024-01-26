ee56414bea113268905cccf4ef05b76b99f44f0e
amandashmore <133187866+amandashmore@users.noreply.github.com>
2024-01-26 17:38:24 -0500

    uncommented stuff

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@ const myImage = document.querySelector("img");
 document.querySelector("html").addEventListener("click", function () {
     alert("Ouch! Stop poking me!");
   });
-//myImage.onclick = () => {
- // const mySrc = myImage.getAttribute("src");
-//  if (mySrc === "images/firefox-icon.png") {
-  //  myImage.setAttribute("src", "images/yellowlab.jpeg");
- // } else {
- //   myImage.setAttribute("src", "images/blacklab.jpeg");
- // }
-//};
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+  myImage.setAttribute("src", "images/yellowlab.jpeg");
+ } else {
+   myImage.setAttribute("src", "images/blacklab.jpeg");
+  }
+};
